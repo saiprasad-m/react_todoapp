adca1a1c4441e6341d77f88b7f81aee320023b63
Sai Prasad <sai.prasad@rocketmail.com>
2019-01-07 16:12:21 +0530

    Function styling ternary operator

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,18 +4,10 @@ import PropTypes from 'prop-types';
 export class TodoItem extends Component {
 
     getStyle = () => {
-        if(this.props.todo.completed) {
-            return {
-                textDecoration: 'line-through'
-            }
-        }
-        else {
-            return {
-                textDecoration: 'none'
-            }
+        return {
+            textDecoration: this.props.todo.completed ? 'line-through' : 'none'
         }
     }
-
 
   render() {
     return (
