924916950352490be681dc74541171f9293853f4
Sai Prasad <sai.prasad@rocketmail.com>
2019-01-07 16:08:37 +0530

    Const styling

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,13 +4,15 @@ import PropTypes from 'prop-types';
 export class TodoItem extends Component {
   render() {
     return (
-      <div style={{
-          backgroundColor: 'lightgrey'
-      }}>
+      <div style={itemStyle}>
         <p>{this.props.todo.title}</p>
       </div>
     )
   }
+}
+
+const itemStyle = {
+    backgroundColor: 'lightgrey'
 }
 
 TodoItem.propTypes = {
