a7355fcaa6ea62ff47b4d4f7ab64cf3dd5543391
Sai Prasad <sai.prasad@rocketmail.com>
2019-01-07 16:09:27 +0530

    Function styling lengthy

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,17 +2,28 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
+
+    getStyle = () => {
+        if(this.props.todo.completed) {
+            return {
+                textDecoration: 'line-through'
+            }
+        }
+        else {
+            return {
+                textDecoration: 'none'
+            }
+        }
+    }
+
+
   render() {
     return (
-      <div style={itemStyle}>
+      <div style={this.getStyle()}>
         <p>{this.props.todo.title}</p>
       </div>
     )
   }
-}
-
-const itemStyle = {
-    backgroundColor: 'lightgrey'
 }
 
 TodoItem.propTypes = {
