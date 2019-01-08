4b1dbb99e11cdd08e68a68d585fd70f52cbc8b3f
Sai Prasad <sai.prasad@rocketmail.com>
2019-01-08 23:07:12 +0530

    removing uuid explicit, addToDo call and jsonplaceholder for todo

--- src/App.js
+++ src/App.js
@@ -9,29 +9,19 @@ import Header from './components/layout/Header';
 import About from './components/pages/About';
 import AddTodo from './components/AddTodo';
 import Todos from './components/Todos';
-import uuid from 'uuid';
+import axios from 'axios';
 
 class App extends Component {
 
   state = {
     todos: [
-      {
-        id: uuid.v4(),
-        title: 'Get Groceris',
-        completed: false
-      },
-      {
-        id: uuid.v4(),
-        title: 'Meet Tech guru',
-        completed: true
-      },
-      {
-        id: uuid.v4(),
-        title: 'Take out the trash',
-        completed: false
-      }
-
     ]
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=6')
+      .then(res => this.setState({ todos: res.data}))
+      .catch(err => console.log(err));
   }
 
   markComplete = (id) => {
@@ -58,14 +48,17 @@ class App extends Component {
   }
 
   addTodo = (title) => {
-    const newTodo = { 
-      id: uuid.v4(),
-      title,
-      conpleted: false
-    }
-    this.setState({
-      todos: [...this.state.todos, newTodo]
+
+    axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed : false
     })
+      .then(res => this.setState({
+        todos: [...this.state.todos, res.data]
+      }))
+      .catch(err => console.log(err));
+
+    
   }
 
   render() {
